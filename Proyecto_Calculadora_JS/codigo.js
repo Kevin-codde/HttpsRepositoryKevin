5505d7fa538693c12eae267fea389fe9491a25f5
unknown <bejaranok537@gmail.com>
2023-10-27 22:52:08 -1200

    mod 27-10-23

--- Proyecto_Calculadora_JS/codigo.js
+++ Proyecto_Calculadora_JS/codigo.js
@@ -13,15 +13,17 @@ boton.addEventListener("click",()=>{
     pant.hidden = false;
     botones = [];
     for(i=0;i<=9;i++){
-     botones[i] = `<button class= "boton" id="b${i}">${i} </button>`
-     botones[10] = `<button class= "boton" id="b${i}">+</button>`
-     botones[12] = `<button class= "boton" id="b${i}">-</button>`
-     botones[13] = `<button class= "boton" id="b${i}">*</button>`
-     botones[14] = `<button class= "boton" id="b${i}">/</button>`
-     botones[11] = `<button class= "boton" id="b${i}">=</button>`
-
+     botones[i] = `<button class= "boton"  id="b${i}" value=${i}>${i}</button>`
+     botones[10] = `<button class= "boton" id="${"+"}" value=${"+"}>+</button>`
+     botones[11] = `<button class= "boton" id="${"-"}" value=${"-"}>-</button>`
+     botones[12] = `<button class= "boton" id="${"*"}" value=${"*"}>*</button>`
+     botones[13] = `<button class= "boton" id="${"/"}" value=${"/"}>/</button>`
+     botones[14] = `<button class= "boton" id="${"="}" value=${"="}>=</button>`
+     
+   
     }
-    cont_b.innerHTML += botones;
+       cont_b.innerHTML += botones;
+
 
 });
 
